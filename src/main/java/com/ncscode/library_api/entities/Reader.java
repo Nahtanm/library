192ad1a93dfd988f5f3fbd1e878db0b9a550beaa
Nahtanm <nahtanmcs0019@gmail.com>
2025-06-30 16:34:05 -0300

    Project and entity-reader created

---
+++ src/main/java/com/ncscode/library_api/entities/Reader.java
@@ -0,0 +1,76 @@
+package com.ncscode.library_api.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Reader implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	private String name;
+	private String email;
+	private LocalDate birthDate;
+
+	public Reader() {
+	}
+
+	public Reader(Integer id, String name, String email, LocalDate birthDate) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reader other = (Reader) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
